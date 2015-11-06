76e289cf00e2f94627c4c868290ef6224ba1f150
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2015-11-06 18:51:58 +0100

    Fully fucntional version, i think

---
+++ README.md
@@ -0,0 +1,13 @@
+Dynamic reconfigure web client
+====
+
+Place this into your robot, run it.
+
+Acces a dynamic reconfigure client just on your browser in any device!
+
+Using REMI as web interface bridge. https://github.com/dddomodossola/remi
+
+sudo pip install https://github.com/dddomodossola/remi/master/master.zip
+
+
+
--- scripts/dyn_reconf_remi.py
+++ scripts/dyn_reconf_remi.py
@@ -33,6 +33,13 @@ class MyApp(App):
         # returning the root widget
         return self.wid
 
+    def dyn_rec_conf_callback(self, new_config):
+        rospy.loginfo("Updating GUI with:\n" + str(new_config))
+        for k in new_config:
+            if self.gui_set_funcs_for_param.has_key(k):
+                for set_func in self.gui_set_funcs_for_param[k]:
+                    set_func(new_config[k])
+
     def on_dropdown_change(self, value):
         print "Dropdown changed to: " + str(value)
         # TODO: Generate the dynamic reconfigure stuff here
@@ -42,9 +49,11 @@ class MyApp(App):
             self.dyn_rec_client.close()
         # Get new client
         # TODO: update all field when config_callback is called
-        self.dyn_rec_client = drc.Client(value, config_callback=None)
+        self.dyn_rec_client = drc.Client(value, timeout=10.0)
+
         # Get a configuration which ensures we'll have the description too
         curr_conf = self.dyn_rec_client.get_configuration()
+        self.gui_set_funcs_for_param = {}
         params_list = self.dyn_rec_client.group_description['parameters']
         if params_list is not None:
             table = gui.Table(DEFAULT_WIDTH, DEFAULT_HEIGHT * len(params_list))
@@ -73,17 +82,20 @@ class MyApp(App):
                     # WTF, really? the full enum dict is actually a string?
                     enum_dict_as_str = param['edit_method']
                     enum_dict = literal_eval(enum_dict_as_str)
-                    print type(enum_dict)
                     description = enum_dict['enum_description']
-                    print "description gotten"
                     current_value = curr_conf[param_name]
-                    print "curr value gotten"
+                    print "CURRENT VALUE OF ENUM IS: " + str(current_value)
                     enums = enum_dict['enum']
-                    print "enums gotten"
-                    enum_wid = self.create_enum_row(param_name, description,
-                                                    current_value,
-                                                    'test_cb',
-                                                    enums)
+                    # Create dynamically a method to be called
+                    method_name = self.add_cb_to_class_by_param_name_and_type(
+                                                                    param_name,
+                                                                    'enum')
+                    enum_wid, set_funcs = self.create_enum_row(param_name,
+                                                               description,
+                                                               current_value,
+                                                               method_name,
+                                                               enums)
+                    self.gui_set_funcs_for_param[param_name] = set_funcs
                     table.append(idx, enum_wid)
                 elif param['type'] == 'int' or param['type'] == 'double':
                     param_name = param['name']
@@ -91,36 +103,86 @@ class MyApp(App):
                     range_max = param['max']
                     description = param['description']
                     current_value = curr_conf[param_name]
-                    # TODO: figure out what callback give it
                     if param['type'] == 'int':
                         step = (range_max - range_min) / 100
                     elif param['type'] == 'double':
                         step = (range_max - range_min) / 100.0
-                    num_wid = self.create_num_row(param_name, description,
-                                                  range_min,
-                                                  range_max, current_value,
-                                                  'test_cb', step)
+
+                    # Create dynamically a method to be called
+                    method_name = self.add_cb_to_class_by_param_name_and_type(
+                                                                    param_name,
+                                                                    'digit')
+
+                    num_wid, set_funcs = self.create_num_row(param_name,
+                                                             description,
+                                                             range_min,
+                                                             range_max,
+                                                             current_value,
+                                                             method_name, step)
+                    self.gui_set_funcs_for_param[param_name] = set_funcs
                     table.append(idx, num_wid)
                 elif param['type'] == 'str':
                     param_name = param['name']
                     current_value = curr_conf[param_name]
                     description = param['description']
-                    # TODO: figure out callback
-                    str_wid = self.create_str_row(param_name, description,
-                                                  current_value,
-                                                  'test_cb')
+                    # Create dynamically a method to be called
+                    method_name = self.add_cb_to_class_by_param_name_and_type(
+                                                                    param_name,
+                                                                    'string')
+                    str_wid, set_funcs = self.create_str_row(param_name,
+                                                             description,
+                                                             current_value,
+                                                             method_name)
+                    self.gui_set_funcs_for_param[param_name] = set_funcs
                     table.append(idx, str_wid)
                 elif param['type'] == 'bool':
                     param_name = param['name']
                     current_value = curr_conf[param_name]
                     description = param['description']
-                    # TODO: figure out callback
-                    bool_wid = self.create_bool_row(param_name, description,
-                                                    current_value,
-                                                    'test_cb')
+                    # Create dynamically a method to be called
+                    method_name = self.add_cb_to_class_by_param_name_and_type(
+                                                                    param_name,
+                                                                    'bool')
+                    bool_wid, set_funcs = self.create_bool_row(param_name,
+                                                               description,
+                                                               current_value,
+                                                               method_name)
+                    self.gui_set_funcs_for_param[param_name] = set_funcs
                     table.append(idx, bool_wid)
-        # TODO: hover text for fields
+
         self.wid.append(2, table)
+
+        # Once the GUI is setup, setup the callback for new configs
+        self.dyn_rec_client.set_config_callback(self.dyn_rec_conf_callback)
+
+    def add_cb_to_class_by_param_name_and_type(self, param_name, param_type):
+        # Create dynamically a method to be called
+        def make_method(parameter_name):
+            def _method(new_value):
+                print "Cb for param '" + parameter_name + "' called."
+                print "We got value: " + str(new_value)
+                if param_type == 'digit' or param_type == 'string':
+                    value_to_set = new_value
+                elif param_type == 'enum':
+                    # Fix the value as its returned as a string and we need to set the int
+                    int_enum = new_value[new_value.find("(")+1:new_value.find(")")]
+                    value_to_set = int(int_enum)
+                elif param_type == 'bool':
+                    # Fix the value as its returned as a string and we need a boolean
+                    if new_value == 'true':
+                        value_to_set = True
+                    elif new_value == 'false':
+                        value_to_set = False
+                    else:
+                        rospy.logerr("Error parsing bool parameter in dynamic callback of " + parameter_name)
+                self.dyn_rec_client.update_configuration({parameter_name: value_to_set})
+
+            return _method
+
+        method_name = "callback_func_for_" + param_name
+        cb_method = make_method(param_name)
+        setattr(self, method_name, cb_method)
+        return method_name
 
     def refresh_servers(self):
         self.dynamic_reconfigure_servers = find_reconfigure_services()
@@ -180,10 +242,14 @@ class MyApp(App):
         item.append(4, spin_val)
         row.append(4, item)
 
-        return row
+        return row, [range_slider.set_value, spin_val.set_value]
 
     def test_cb(self, *args):
         rospy.loginfo("We got a cb with args: " + str(args))
+        rospy.loginfo("Which the arg is of type: ")
+        if len(args) >= 1:
+            rospy.loginfo(str(type(args[0])))
+        #self.dyn_rec_client.update_configuration()
 
     def create_str_row(self, param_name, description,
                        current_value, callback_cb_name):
@@ -197,8 +263,8 @@ class MyApp(App):
 
         text_input.set_on_enter_listener(self, callback_cb_name)
 
-        set_button = gui.Button(80, DEFAULT_HEIGHT, text="Set")
-        set_button.set_on_click_listener(self, callback_cb_name)
+        # set_button = gui.Button(80, DEFAULT_HEIGHT, text="Set")
+        # set_button.set_on_click_listener(self, callback_cb_name)
 
         item = gui.TableItem()
         item.append(0, param_name)
@@ -219,10 +285,11 @@ class MyApp(App):
         row.append(3, item)
 
         item = gui.TableItem()
-        item.append(4, set_button)
+        #item.append(4, set_button)
+        item.append(4, "")
         row.append(4, item)
 
-        return row
+        return row, [text_input.set_text]
 
     def create_bool_row(self, param_name, description,
                         current_value, callback_cb_name):
@@ -257,7 +324,7 @@ class MyApp(App):
         item.append(4, "")
         row.append(4, item)
 
-        return row
+        return row, [checkbox.set_value]
 
     def create_enum_row(self, param_name, description, current_value,
                         callback_cb_name, enums):
@@ -276,11 +343,12 @@ class MyApp(App):
             value = enum['value']
             name = enum['name']
             type_enum = enum['type']
-            print "pre ddi"
             ddi = gui.DropDownItem(300, DEFAULT_HEIGHT,
                                    name + " (" + str(value) + ")")
             ddi.attributes['title'] = description_enum
             dropdown.append(idx, ddi)
+
+        dropdown.select_by_key(current_value)
 
         item = gui.TableItem()
         item.append(0, param_name)
@@ -305,12 +373,13 @@ class MyApp(App):
         item.append(4, "")
         row.append(4, item)
 
-        return row
+        # return the row itself and the setter func list
+        return row, [dropdown.set_value]
 
 
 if __name__ == "__main__":
     rospy.init_node('web_dyn_reconf')
-    #server.DEBUG_MODE = 2
-    app = server.Server(MyApp, start=True, port=8081,
+    server.DEBUG_MODE = 2
+    app = server.Server(MyApp, start=True, address="192.168.200.132", port=8081,
                         multiple_instance=True)
     rospy.spin()
